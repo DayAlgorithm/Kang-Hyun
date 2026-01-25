fda9d5980c0a561605d458801fe8f6a68ac0574c
HyunWrites0721 <maxsky9018@gmail.com>
2026-01-25 13:53:04 +0900

    자유문제

---
+++ 8.자유롭게/260125_BOJ_두_용액.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+solutions = list(map(int, input().split()))
+solutions = sorted(solutions)
+ans1, ans2 = solutions[0], solutions[n-1]
+ans_sum = abs(ans1 + ans2)
+lo, hi = 0, n-1
+
+while lo < hi:
+    s1, s2 = solutions[lo], solutions[hi]
+    cur_sum = s1 + s2
+    if abs(cur_sum) < ans_sum:
+        ans1, ans2 = s1, s2
+        ans_sum = abs(cur_sum)
+    if cur_sum < 0:
+        lo +=1
+    elif cur_sum == 0:
+        break
+    else:
+        hi -= 1
+
+print(ans1, ans2)
